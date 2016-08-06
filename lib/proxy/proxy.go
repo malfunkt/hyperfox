7c71ec0c152fd4a79e7ce2f6e211076d48065621
José Carlos Nieto <jose.carlos@menteslibres.net>
2016-08-06 08:40:21 -0500

    Refactor and updating tests

---
+++ lib/proxy/proxy.go
@@ -0,0 +1,332 @@
+// Copyright (c) 2012-today José Carlos Nieto, https://menteslibres.net/xiam
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// Package proxy provides methods for creating a proxy programmatically.
+package proxy
+
+import (
+	"bytes"
+	"crypto/tls"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/xiam/hyperfox/lib/gencert"
+)
+
+const (
+	// EnvSSLKey defines the name for the environment variable that holds the
+	// root SSL key.
+	EnvSSLKey = `HYPERFOX_SSL_KEY`
+	// EnvSSLCert defines the name for the environment variable that holds the
+	// root SSL certificate..
+	EnvSSLCert = `HYPERFOX_SSL_CERT`
+)
+
+// BodyWriteCloser interface returns a io.WriteCloser where a copy of the
+// response body will be written. The io.WriteCloser's Close() method will be
+// called after the body has been written entirely.
+//
+// destination -> ... -> BodyWriteCloser -> client -> ...
+type BodyWriteCloser interface {
+	NewWriteCloser(*http.Response) (io.WriteCloser, error)
+}
+
+// Director interface gets a reference of the http.Request sent by an user
+// before sending it to the destination. The Direct() method may modify the
+// client's request.
+//
+// client -> Director -> destination
+type Director interface {
+	Direct(*http.Request) error
+}
+
+// Interceptor interface gets a reference of the http.Response sent by the
+// destination before arriving to the client. The Interceptor() method may
+// modify the destination's response.
+//
+// destination -> Interceptor -> ... -> client -> ...
+type Interceptor interface {
+	Intercept(*http.Response) error
+}
+
+// Logger interface gets a reference of the ProxiedRequest after the response
+// has been writte to the client.
+//
+// The Logger() method must not modify any *ProxiedRequest properties.
+//
+// destination -> ... -> client -> Logger
+type Logger interface {
+	Log(*ProxiedRequest) error
+}
+
+// Proxy struct provides methods and properties for creating a proxy
+// programatically.
+type Proxy struct {
+	// Standard HTTP server
+	srv http.Server
+	// Writer functions.
+	writers []BodyWriteCloser
+	// Director functions.
+	directors []Director
+	// Interceptor functions.
+	interceptors []Interceptor
+	// Logger functions.
+	loggers []Logger
+}
+
+// ProxiedRequest struct provides properties for executing a *http.Request and
+// proxying it into a http.ResponseWriter.
+type ProxiedRequest struct {
+	ResponseWriter http.ResponseWriter
+	Request        *http.Request
+	Response       *http.Response
+}
+
+// NewProxy creates and returns a Proxy reference.
+func NewProxy() *Proxy {
+	return &Proxy{}
+}
+
+// Reset clears the list of interfaces.
+func (p *Proxy) Reset() {
+	p.writers = []BodyWriteCloser{}
+	p.directors = []Director{}
+	p.interceptors = []Interceptor{}
+	p.loggers = []Logger{}
+}
+
+// NewProxiedRequest creates and returns a ProxiedRequest reference.
+func (p *Proxy) newProxiedRequest(w http.ResponseWriter, r *http.Request) *ProxiedRequest {
+	return &ProxiedRequest{
+		ResponseWriter: w,
+		Request:        r,
+	}
+}
+
+// AddBodyWriteCloser appends a struct that satisfies the BodyWriteCloser
+// interface to the list of body write closers.
+func (p *Proxy) AddBodyWriteCloser(wc BodyWriteCloser) {
+	p.writers = append(p.writers, wc)
+}
+
+// AddDirector appends a struct that satisfies the Director interface to the
+// list of directors.
+func (p *Proxy) AddDirector(dir Director) {
+	p.directors = append(p.directors, dir)
+}
+
+// AddInterceptor appends a struct that satisfies the Interceptor interface to
+// the list of interceptors.
+func (p *Proxy) AddInterceptor(dir Interceptor) {
+	p.interceptors = append(p.interceptors, dir)
+}
+
+// AddLogger appends a struct that satisfies the Logger interface to the list
+// of loggers.
+func (p *Proxy) AddLogger(dir Logger) {
+	p.loggers = append(p.loggers, dir)
+}
+
+// copyHeader copies headers from one http.Header to another.
+// http://golang.org/src/pkg/net/http/httputil/reverseproxy.go#L72
+func copyHeader(dst http.Header, src http.Header) {
+	for k := range dst {
+		dst.Del(k)
+	}
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
+}
+
+// ServeHTTP catches a client request and proxies it to the destination server,
+// then waits for the server's answer and sends it back to the client.
+//
+// (this method should not be called directly).
+func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	pr := p.newProxiedRequest(w, r)
+
+	out := new(http.Request)
+	// Copy request.
+	*out = *r
+
+	var transport *http.Transport
+	if r.TLS == nil {
+		transport = &http.Transport{}
+		out.URL.Scheme = "http"
+	} else {
+		transport = &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: false,
+			},
+		}
+		out.URL.Scheme = "https"
+	}
+
+	// Making sure the Host header is present.
+	out.URL.Host = out.Host
+	out.Header.Add("Host", out.Host)
+
+	out.Proto = "HTTP/1.1"
+	out.ProtoMajor = 1
+	out.ProtoMinor = 1
+	out.Close = false
+
+	// Walking over directors.
+	for i := range p.directors {
+		if err := p.directors[i].Direct(out); err != nil {
+			log.Printf("Director: %q", err)
+		}
+	}
+
+	// Intercepting request body.
+	body := bytes.NewBuffer(nil)
+	bodyCopy := bytes.NewBuffer(nil)
+
+	if out.Body != nil {
+		io.Copy(io.MultiWriter(body, bodyCopy), out.Body)
+		out.Body.Close()
+		out.Body = ioutil.NopCloser(body)
+	}
+
+	// Proxying client request to destination server.
+	var err error
+	if pr.Response, err = transport.RoundTrip(out); err != nil {
+		log.Printf("RoundTrip: %q", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	defer pr.Response.Body.Close()
+
+	// (Response received).
+
+	// Resetting body (so it can be read later)
+	if out.Body != nil {
+		out.Body = ioutil.NopCloser(bodyCopy)
+	}
+
+	// Walking over interceptos.
+	for i := range p.interceptors {
+		if err := p.interceptors[i].Intercept(pr.Response); err != nil {
+			log.Printf("Interceptor: %q", err)
+		}
+	}
+
+	// Copying response headers to the writer we are going to send to the client.
+	copyHeader(pr.ResponseWriter.Header(), pr.Response.Header)
+
+	// Copying response status.
+	pr.ResponseWriter.WriteHeader(pr.Response.StatusCode)
+
+	// Running writers.
+	ws := make([]io.WriteCloser, 0, len(p.writers))
+
+	for i := range p.writers {
+		var w io.WriteCloser
+		var err error
+		if w, err = p.writers[i].NewWriteCloser(pr.Response); err != nil {
+			log.Printf("WriteCloser: %q", err)
+			continue
+		}
+		ws = append(ws, w)
+	}
+
+	// Writing response.
+	writers := make([]io.Writer, 0, len(ws)+1)
+	writers = append(writers, pr.ResponseWriter)
+
+	for i := range ws {
+		writers = append(writers, ws[i])
+	}
+
+	if _, err := io.Copy(io.MultiWriter(writers...), pr.Response.Body); err != nil {
+		log.Printf("io.Copy: %q", err)
+	}
+
+	// Closing write closers.
+	for i := range ws {
+		if err := ws[i].Close(); err != nil {
+			log.Printf("WriteCloser.Close: %q", err)
+		}
+	}
+
+	// Walking over loggers.
+	for i := range p.loggers {
+		if err := p.loggers[i].Log(pr); err != nil {
+			log.Printf("Log: %q", err)
+		}
+	}
+}
+
+// Start creates an HTTP proxy server that listens on the given address.
+func (p *Proxy) Start(addr string) error {
+	p.srv = http.Server{
+		Addr:    addr,
+		Handler: p,
+	}
+
+	log.Printf("Listening for incoming HTTP client requests on %s.\n", addr)
+	if err := p.srv.ListenAndServe(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func certificateLookup(clientHello *tls.ClientHelloInfo) (*tls.Certificate, error) {
+	cert, key, err := gencert.CreateKeyPair(clientHello.ServerName)
+	if err != nil {
+		return nil, err
+	}
+
+	var tlsCert tls.Certificate
+	if tlsCert, err = tls.LoadX509KeyPair(cert, key); err != nil {
+		return nil, err
+	}
+
+	return &tlsCert, nil
+}
+
+// StartTLS creates an HTTPs proxy server that listens on the given address.
+func (p *Proxy) StartTLS(addr string) error {
+	p.srv = http.Server{
+		Addr:    addr,
+		Handler: p,
+		TLSConfig: &tls.Config{
+			GetCertificate:     certificateLookup,
+			InsecureSkipVerify: false,
+		},
+	}
+
+	log.Printf("Listening for incoming HTTPs client requests on %s.\n", addr)
+
+	gencert.SetRootCACert(os.Getenv(EnvSSLCert))
+	gencert.SetRootCAKey(os.Getenv(EnvSSLKey))
+
+	if err := p.srv.ListenAndServeTLS(os.Getenv(EnvSSLCert), os.Getenv(EnvSSLKey)); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ lib/proxy/proxy_test.go
@@ -0,0 +1,316 @@
+package proxy
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"testing"
+	"time"
+)
+
+var (
+	proxy    *Proxy
+	sslProxy *Proxy
+)
+
+const (
+	listenHTTPAddr  = `127.0.0.1:13080`
+	listenHTTPsAddr = `127.0.0.1:13443`
+)
+
+type writeCloser struct {
+	bytes.Buffer
+	closed bool
+}
+
+func (w *writeCloser) Close() error {
+	w.closed = true
+	return nil
+}
+
+type testLogger struct {
+	logged bool
+}
+
+func (l *testLogger) Log(pr *ProxiedRequest) error {
+	l.logged = true
+	return nil
+}
+
+type testWriteCloser struct {
+	wc *writeCloser
+}
+
+func (w *testWriteCloser) NewWriteCloser(*http.Response) (io.WriteCloser, error) {
+	w.wc = &writeCloser{}
+	return w.wc, nil
+}
+
+type testInterceptor struct {
+}
+
+func (i testInterceptor) Intercept(res *http.Response) error {
+	var err error
+	var buf []byte
+
+	// Forging response status.
+	res.StatusCode = 500
+
+	// Reading response.
+	if buf, err = ioutil.ReadAll(res.Body); err != nil {
+		return err
+	}
+
+	// Modifying response.
+	buf = bytes.Replace(buf, []byte("nmap.org"), []byte("mapn.tld"), -1)
+
+	// Replacing response body.
+	res.Body = ioutil.NopCloser(bytes.NewBuffer(buf))
+
+	return nil
+}
+
+type testDirectorSSL struct {
+}
+
+func (d testDirectorSSL) Direct(req *http.Request) error {
+	newRequest, _ := http.NewRequest("GET", "https://www.example.org/", nil)
+	*req = *newRequest
+	return nil
+}
+
+type testDirector struct {
+}
+
+func (d testDirector) Direct(req *http.Request) error {
+	newRequest, _ := http.NewRequest("GET", "https://nmap.org/", nil)
+	*req = *newRequest
+	return nil
+}
+
+type testResponseWriter struct {
+	header http.Header
+	buf    *bytes.Buffer
+	status int
+}
+
+func (rw *testResponseWriter) Header() http.Header {
+	return rw.header
+}
+
+func (rw *testResponseWriter) Write(buf []byte) (int, error) {
+	return rw.buf.Write(buf)
+}
+
+func (rw *testResponseWriter) WriteHeader(i int) {
+	rw.status = i
+}
+
+func newTestResponseWriter() *testResponseWriter {
+	rw := &testResponseWriter{
+		header: http.Header{},
+		buf:    bytes.NewBuffer(nil),
+	}
+	return rw
+}
+
+func TestListenHTTP(t *testing.T) {
+	proxy = NewProxy()
+
+	go func(t *testing.T) {
+		if err := proxy.Start(listenHTTPAddr); err != nil {
+			t.Fatal(err)
+		}
+	}(t)
+
+	time.Sleep(time.Millisecond * 100)
+}
+
+func TestListenHTTPs(t *testing.T) {
+	sslProxy = NewProxy()
+
+	go func(t *testing.T) {
+		if err := sslProxy.StartTLS(listenHTTPsAddr); err != nil {
+			t.Fatal(err)
+		}
+	}(t)
+
+	time.Sleep(time.Millisecond * 100)
+}
+
+func TestProxyResponse(t *testing.T) {
+	var req *http.Request
+	var err error
+
+	// Creating a request.
+	if req, err = http.NewRequest("GET", "https://www.example.org", nil); err != nil {
+		t.Fatal(err)
+	}
+	req.TransferEncoding = []string{"identity"}
+
+	// Creating a response writer.
+	wri := newTestResponseWriter()
+
+	// Executing request.
+	proxy.ServeHTTP(wri, req)
+
+	// Verifying response.
+	if wri.header.Get("Date") == "" {
+		t.Fatal("Expecting a date.")
+	}
+}
+
+func TestDirectorInterface(t *testing.T) {
+	var req *http.Request
+	var err error
+
+	// Creating a request to golang.org
+	if req, err = http.NewRequest("GET", "http://www.golang.org", nil); err != nil {
+		t.Fatal(err)
+	}
+
+	// Creating a response writer.
+	wri := newTestResponseWriter()
+
+	// Adding a director that will change the request destination to insecure.org
+	proxy.AddDirector(testDirector{})
+
+	// Executing request.
+	proxy.ServeHTTP(wri, req)
+
+	if bytes.Count(wri.buf.Bytes(), []byte(`nmap.org`)) == 0 {
+		t.Fatal("Director failed to take over the request.")
+	}
+}
+
+func TestInterceptorInterface(t *testing.T) {
+	var req *http.Request
+	var err error
+
+	// Creating a request to golang.org
+	if req, err = http.NewRequest("GET", "http://www.golang.org", nil); err != nil {
+		t.Fatal(err)
+	}
+
+	// Creating a response writer.
+	wri := newTestResponseWriter()
+
+	// Adding an interceptos that will alter the response status and some texts
+	// from the original page.
+	proxy.AddInterceptor(testInterceptor{})
+
+	// Executing request.
+	proxy.ServeHTTP(wri, req)
+
+	if wri.status != 500 {
+		t.Fatal("Expecting status change.")
+	}
+
+	if bytes.Count(wri.buf.Bytes(), []byte("mapn.tld")) == 0 {
+		t.Fatal("Interceptor failed to modify the response.")
+	}
+}
+
+func TestBodyWriteCloserInterface(t *testing.T) {
+	var req *http.Request
+	var err error
+
+	// Creating a request to golang.org
+	if req, err = http.NewRequest("GET", "http://www.golang.org", nil); err != nil {
+		t.Fatal(err)
+	}
+
+	// Creating a response writer.
+	wri := newTestResponseWriter()
+
+	// Adding write closer that will receive all the data and then a closing
+	// instruction.
+	w := &testWriteCloser{}
+	proxy.AddBodyWriteCloser(w)
+
+	// Executing request.
+	proxy.ServeHTTP(wri, req)
+
+	if wri.status != 500 {
+		t.Fatal("Expecting status change.")
+	}
+
+	if bytes.Count(wri.buf.Bytes(), []byte("mapn.tld")) == 0 {
+		t.Fatal("Interceptor failed to modify the response.")
+	}
+
+	if bytes.Equal(w.wc.Bytes(), wri.buf.Bytes()) == false {
+		t.Fatal("Buffers must be equal.")
+	}
+
+	if w.wc.closed == false {
+		t.Fatal("WriteCloser must be closed.")
+	}
+}
+
+func TestLoggerInterface(t *testing.T) {
+	var req *http.Request
+	var err error
+
+	// Creating a request to golang.org
+	if req, err = http.NewRequest("GET", "http://www.example.org", nil); err != nil {
+		t.Fatal(err)
+	}
+
+	// Creating a response writer.
+	wri := newTestResponseWriter()
+
+	// Adding write closer that will receive all the data and then a closing
+	// instruction.
+	log := &testLogger{}
+	proxy.AddLogger(log)
+
+	// Executing request.
+	proxy.ServeHTTP(wri, req)
+
+	if log.logged == false {
+		t.Fatal("Expected flag change.")
+	}
+}
+
+func TestActualHTTPClient(t *testing.T) {
+	// Reset lists.
+	proxy.Reset()
+
+	// Adding a director that will change the request destination to insecure.org
+	proxy.AddDirector(testDirector{})
+
+	// Adding write closer that will receive all the data and then a closing
+	// instruction.
+	w := &testWriteCloser{}
+	proxy.AddBodyWriteCloser(w)
+
+	client := &http.Client{}
+	res, err := client.Get("http://" + listenHTTPAddr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if bytes.Count(w.wc.Bytes(), []byte("nmap.org")) < 1 {
+		t.Fatal("Expecting a redirection.")
+	}
+
+	buf := bytes.NewBuffer(nil)
+	if _, err := io.Copy(buf, res.Body); err != nil {
+		t.Fatal(err)
+	}
+
+	if bytes.Equal(buf.Bytes(), w.wc.Bytes()) == false {
+		t.Fatal("Responses differ.")
+	}
+}
+
+func SkipTestHTTPsDirectorInterface(t *testing.T) {
+	sslProxy.Reset()
+	// Adding a director that will change the request destination to insecure.org
+	sslProxy.AddDirector(testDirectorSSL{})
+	log.Printf("SSL proxy server will be open for 10 secs from now...")
+	time.Sleep(time.Second * 10)
+}
