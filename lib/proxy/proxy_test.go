9d67f08b916b0053a8e83d2a2eebecd1a04a83a1
Jack Thomasson <thomasso@adobe.com>
2016-11-08 17:17:23 -0700

    use httpunix to proxy HTTP over Unix socket Example: hyperfox -p 0 -S /var/run/docker.sock DOCKER_HOST=/tmp/hyperfox docker ps

--- lib/proxy/proxy_test.go
+++ lib/proxy/proxy_test.go
@@ -5,19 +5,27 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
+	"os"
 	"testing"
 	"time"
+
+	"github.com/tv42/httpunix"
 )
 
 var (
-	proxy    *Proxy
-	sslProxy *Proxy
+	proxy      *Proxy
+	sslProxy   *Proxy
+	unixServer *UnixServer
+	unixProxy  *Proxy
 )
 
 const (
 	listenHTTPAddr  = `127.0.0.1:13080`
 	listenHTTPsAddr = `127.0.0.1:13443`
+	listenUnixPath  = `/tmp/test_proxy`
+	serverUnixPath  = `/tmp/test_server`
 )
 
 type writeCloser struct {
@@ -140,6 +148,51 @@ func TestListenHTTPs(t *testing.T) {
 	time.Sleep(time.Millisecond * 100)
 }
 
+type UnixServer struct {
+	http.Server
+}
+
+func NewUnixServer() *UnixServer {
+	return &UnixServer{}
+}
+
+func (s *UnixServer) ServeHTTP(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("OK"))
+}
+
+func TestUnixServer(t *testing.T) {
+	unixServer = NewUnixServer()
+	unixServer.Addr = "http+unix://" + serverUnixPath
+	unixServer.Handler = unixServer
+
+	go func(t *testing.T) {
+		os.Remove(serverUnixPath)
+		l, err := net.Listen("unix", serverUnixPath)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer l.Close()
+		defer os.Remove(serverUnixPath)
+		if err := unixServer.Serve(l); err != nil {
+			t.Fatal(err)
+		}
+	}(t)
+
+	time.Sleep(time.Millisecond * 100)
+}
+
+func TestListenUnix(t *testing.T) {
+	unixProxy = NewProxy()
+
+	go func(t *testing.T) {
+		if err := unixProxy.StartUnix(listenUnixPath, serverUnixPath); err != nil {
+			t.Fatal(err)
+		}
+	}(t)
+
+	time.Sleep(time.Millisecond * 100)
+}
+
 func TestProxyResponse(t *testing.T) {
 	var req *http.Request
 	var err error
@@ -197,7 +250,7 @@ func TestInterceptorInterface(t *testing.T) {
 	// Creating a response writer.
 	wri := newTestResponseWriter()
 
-	// Adding an interceptos that will alter the response status and some texts
+	// Adding an interceptor that will alter the response status and some texts
 	// from the original page.
 	proxy.AddInterceptor(testInterceptor{})
 
@@ -307,6 +360,31 @@ func TestActualHTTPClient(t *testing.T) {
 	}
 }
 
+func TestUnixProxy(t *testing.T) {
+	// Adding write closer that will receive all the data and then a closing
+	// instruction.
+	w := &testWriteCloser{}
+	proxy.AddBodyWriteCloser(w)
+
+	u := &httpunix.Transport{}
+	u.RegisterLocation("proxy", listenUnixPath)
+	client := http.Client{
+		Transport: u,
+	}
+	res, err := client.Get("http+unix://proxy")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	buf := bytes.NewBuffer(nil)
+	if _, err := io.Copy(buf, res.Body); err != nil {
+		t.Fatal(err)
+	}
+	if buf.String() != "OK" {
+		t.Fatal("Expected OK, got", buf)
+	}
+}
+	
 func SkipTestHTTPsDirectorInterface(t *testing.T) {
 	sslProxy.Reset()
 	// Adding a director that will change the request destination to insecure.org
