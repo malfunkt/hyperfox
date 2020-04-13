aa6dd69da7692b3cd5cf4ba1d079ca77b81a63b9
José Nieto <jose@xiam.io>
2020-04-12 19:41:39 -0500

    update install file

--- install.sh
+++ install.sh
@@ -51,7 +51,7 @@ download() {
     echo "Could not install." && exit 1
   fi;
 
-  sudo install -c -m 0755 $FILENAME $BIN_DIR/hyperfox || echo "This script needs root privileges in order to install into $BIN_DIR."
+  sudo install -v -c -m 0755 $FILENAME $BIN_DIR/hyperfox || echo "This script needs root privileges in order to install into $BIN_DIR."
   rm $FILENAME
 }
 
